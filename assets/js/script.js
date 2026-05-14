d35e51a3af0e8b22cfca3f3ffaeb083bfea2ca6c
tanishqshrivastava-cs <tanishqshrivastava.cs@gmail.com>
2026-05-14 19:16:09 +0530

    update portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,19 +141,30 @@ const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
+// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
+
   navigationLinks[i].addEventListener("click", function () {
 
+    const targetPage = this.dataset.navTarget;
+
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+
+      if (targetPage === pages[i].dataset.page) {
+
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
+
       } else {
+
         pages[i].classList.remove("active");
         navigationLinks[i].classList.remove("active");
+
       }
+
     }
 
   });
+
 }
